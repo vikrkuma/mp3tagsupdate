674f32903cdd34092893a4b505bfb2d1044f4f88
Vikrant <vikrant.coder@gmail.com>
2023-02-13 17:10:36 +0530

    feat: add code for mp3 tags update

---
+++ index.js
@@ -0,0 +1,143 @@
+const fs = require("fs");
+const path = require("path");
+const NodeID3 = require("node-id3");
+
+const srcDir = "/Users/vikrant/Desktop/MP3/Punjabi";
+const dstDir = "./Converted/Punjabi/";
+const albumName = "Punjabi Songs";
+
+const DEBUG = false;
+const SORT_BY_CREATION_DATE = false;
+
+let fileNumber = 1;
+
+function addFileDetails(file) {
+  const filePath = path.join(srcDir, file);
+  return {
+    file,
+    path: filePath,
+    ext: path.extname(file),
+    stat: fs.statSync(filePath),
+  };
+}
+
+function filterMp3Files({ ext }) {
+  return Boolean(ext === ".mp3");
+}
+
+function sortFilesByCreationDate(fileObjA, fileObjB) {
+  const creationTimeOfFileA = new Date(fileObjA.stat.mtime);
+  const creationTimeOfFileB = new Date(fileObjB.stat.mtime);
+  return creationTimeOfFileA - creationTimeOfFileB;
+}
+
+function capitalize(str) {
+  return str
+    .split(" ")
+    .map((word) => `${word.substr(0, 1).toUpperCase()}${word.substr(1)}`)
+    .join(" ");
+}
+
+function copyFile(fileObj) {
+  const newFileName = getNewFileName(fileObj.file, fileObj.ext);
+  const dstFile = `${dstDir}${newFileName}`;
+  if (!DEBUG) {
+    const tags = {};
+    tags.trackNumber = fileNumber - 1;
+    tags.title = newFileName.substr(0, newFileName.length - 4);
+    tags.album = albumName;
+    tags.performerInfo = "";
+    tags.composer = "";
+    tags.comment = {};
+    tags.performerInfo ="";
+    tags.composer = "";
+    tags.remixArtist = "";
+    tags.partOfSet = "";
+    tags.userDefinedText = [];
+    tags.commercialFrame = [];
+    tags.albumSortOrder = "";
+    tags.performerSortOrder = "";
+    tags.titleSortOrder = "";
+    tags.userDefinedText = "";
+    tags.description = "";
+    tags.contentGroup = "";
+    tags.subtitle = "";
+    tags.genre = albumName;
+    fs.copyFileSync(fileObj.path, dstFile);
+    NodeID3.update(tags, dstFile, () => {});
+  }
+  console.log(newFileName);
+}
+
+function getNewFileName(file, ext) {
+  const fileName = file
+    .replace(ext, "")
+    .replace(new RegExp("--", "g"), "-")
+    .replace(new RegExp("__", "g"), "-")
+    .replace(new RegExp("_", "g"), "-")
+    .replace(new RegExp("-", "g"), " ")
+    .replace("Songspkred.co", "")
+    .replace("(DjPunjab.Com)", "")
+    .replace("(Pagalworld.pw)", "")
+    .replace("(PagalWorld)", "")
+    .replace("(PaglaSongs)", "")
+    .replace("[PagalWorld.NL]", "")
+    .replace("128 kbps sound", "")
+    .replace("128 Kbps", "")
+    .replace("CKNG", "")
+    .replace("www.oldisgold.co", "")
+    .replace(".in", "")
+    .replace("[wwwDJMazaCom]", "")
+    .replace("[www.DJMaza.Com]", "")
+    .replace("[Songs.PK]", "")
+    .replace("[DJMazaInfo]", "")
+    .replace("(DjRaagNet)", "")
+    .replace("(PagalWorldComCom)", "")
+    .replace("[SongsPk.CC]", "")
+    .replace("(RaagSong.Com)", "")
+    .replace("[DJMaza.Info]", "")
+    .replace("[www.Mp3MaD.Com]", "")
+    .replace("DownloadMing.SE", "")
+    .replace("[IndiaStar.Tk]", "")
+    .replace("[DDR]", "")
+    .replace("(RaagJatt.com)", "")
+    .replace("(Male Version)", "")
+    .replace("(KoshalWorld.Com)", "")
+    .replace("(Mr Jatt.com)", "")
+    .replace("[MyMp3Bhojpuri.In]", "")
+    .replace("(DjRaag.Net)", "")
+    .replace("[aR]","")
+    .replace("(PagalWorldCom.Com)", "")
+    .replace("Kbps", "")
+    .replace("320kbps", "")
+    .replace("(amlijatt)", "")
+    .replace(new RegExp("[0-9]", "g"), "")
+    .replace("()", "")
+    .replace("[]", "")
+    .replace(".", "")
+    .replace(/  +/g, " ")
+    .trim();
+  const fileNumberString = `${fileNumber++}`.padStart(3, "0");
+  return `${fileNumberString} ${capitalize(fileName)}${ext}`;
+}
+
+async function main() {
+  fs.mkdirSync(dstDir, {recursive: true});
+  const files = fs
+    .readdirSync(srcDir)
+    .map(addFileDetails)
+    .filter(filterMp3Files);
+
+    if (SORT_BY_CREATION_DATE) {
+      files.sort(sortFilesByCreationDate);
+    }
+
+  for (const fileObj of files) {
+    copyFile(fileObj);
+    if (!DEBUG) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
+}
+
+main();
